47757fa6437a5dbf1ec1863f4c40fdde252a6afa
notroot <notroot@email.server>
2017-02-18 09:01:40 -0800

    update

--- Module1/testfilenew.java
+++ Module1/testfilenew.java
@@ -7,7 +7,8 @@ public class testfilenew
 		System.out.println("REMOVED: by notroot on Mint");
 		System.out.println("REMOVED: by notroot on MAC");
 		System.out.println("REMOVED: by notroot on Mint, to something different");
-		System.out.println("ADDED: by notroot on little Vaio");
+		System.out.println("REMOVED: by notroot on little Vaio");
+		System.out.println("ADDED: individual workspace and git repositories");
 	}
 	
 }
