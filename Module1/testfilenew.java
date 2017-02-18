6f6ecaf0eeb93a280c17bab47662154ba46c13ff
notroot <notroot@email.server>
2017-02-18 08:50:27 -0800

    upload

--- Module1/testfilenew.java
+++ Module1/testfilenew.java
@@ -6,7 +6,8 @@ public class testfilenew
 		System.out.println("REMOVED: Edited on little Vaio");
 		System.out.println("REMOVED: by notroot on Mint");
 		System.out.println("REMOVED: by notroot on MAC");
-		System.out.println("CHANGED: by notroot on Mint, to something different");
+		System.out.println("REMOVED: by notroot on Mint, to something different");
+		System.out.println("ADDED: by notroot on little Vaio");
 	}
 	
 }
