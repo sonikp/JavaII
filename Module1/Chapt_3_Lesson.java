fbe80b005d74d9270115c8cf6219b11ac6e9bd8e
notroot <notroot@email.server>
2017-02-18 10:37:46 -0800

    update

--- Module1/Chapt_3_Lesson.java
+++ Module1/Chapt_3_Lesson.java
@@ -1,7 +1,64 @@
-public class Chapt_3_Lesson
+public class Chapt_3_Lesson extends Turtle
 {
+	
+	// constructors
+	public Chapt_3_Lesson(int x, int y, ModelDisplay modelDisplayer) 
+	{
+		super(x, y, modelDisplayer);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void drawSquare()
+	{
+		this.forward(200);
+		this.turnRight();
+		this.forward(200);
+		this.turnRight();
+		this.forward(200);
+		this.turnRight();
+		this.forward(200);
+		this.turnRight();
+	
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args)
 	{
-		System.out.println("Chapt_3_Lesson");
+		// 3.3.2
+		System.out.println("New world ..." + new World());
+		World worldObj = new World();
+		System.out.println(worldObj);
+		Turtle turtle1 = new Turtle(150,200,worldObj);
+		Turtle turtle2 = new Turtle(350,200,worldObj);
+		Turtle turtle3 = new Turtle(250,300,worldObj);
+		System.out.println(turtle1 + "\n" + turtle2);
+		
+		turtle1.forward(100);
+		turtle1.turnRight();
+		turtle1.forward(100);
+		turtle1.turnRight();
+		turtle1.forward(100);
+		turtle1.turnRight();
+		turtle1.forward(100);
+		turtle1.turnRight();
+		
+		turtle2.backward(100);
+		turtle2.turnLeft();
+		turtle2.backward(100);
+		turtle2.turnLeft();
+		turtle2.backward(100);
+		turtle2.turnLeft();
+		turtle2.backward(100);
+		turtle2.turnLeft();
+		
+		Chapt_3_Lesson localObj = new Chapt_3_Lesson(250,100,worldObj);
+		localObj.drawSquare();
+		
+		
+		
+		
 	}
 }
--- Module1/testfilenew.java
+++ Module1/testfilenew.java
@@ -3,12 +3,7 @@ public class testfilenew
 	public static void main(String[] args)
 	{
 		System.out.println("testfilenew.java in Module 1");
-		System.out.println("REMOVED: Edited on little Vaio");
-		System.out.println("REMOVED: by notroot on Mint");
-		System.out.println("REMOVED: by notroot on MAC");
-		System.out.println("REMOVED: by notroot on Mint, to something different");
-		System.out.println("REMOVED: by notroot on little Vaio");
-		System.out.println("ADDED: individual workspace and git repositories");
+		System.out.println("CLEANED");
 	}
 	
 }
