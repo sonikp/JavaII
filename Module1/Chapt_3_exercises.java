1f45aaf0967c334af1b04c6729bf72dfdde39219
notroot <notroot@email.server>
2017-02-18 12:48:23 -0800

    update

--- Module1/Chapt_3_exercises.java
+++ Module1/Chapt_3_exercises.java
@@ -91,7 +91,9 @@ public class Chapt_3_exercises extends Turtle
 	{
 		System.out.println("REMOVED: random change");
 		System.out.println("REMOVED: Another random change");
-		System.out.println("ADDED: A (NEW) random change");
+		System.out.println("REMOVED: Another random change");
+		System.out.println("REMOVED: Another random change");
+		System.out.println("ADDED: Another (NEW) random change");
 		World starWorld = new World();
 		Chapt_3_exercises starTurtle = new Chapt_3_exercises(320,240,starWorld);
 		starTurtle.drawStar(100, 144, 72);
